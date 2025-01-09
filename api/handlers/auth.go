d82961bd343a2479c83a8bb2f3c0d0a84c5075bd
Clarkden <clarkh_@outlook.com>
2025-01-08 22:45:42 -0800

    Add user registration functionality and response handling; create users table migration

--- api/handlers/auth.go
+++ api/handlers/auth.go
@@ -1,7 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
+	sugar "sugar/data"
+	"sugar/helpers/response"
 )
 
 func (h *Handler) HandleEmailLogin() http.HandlerFunc {
@@ -13,5 +17,39 @@ func (h *Handler) HandleEmailLogin() http.HandlerFunc {
 func (h *Handler) HandleEmailRegister() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
+		type EmailRegisterRequest struct {
+			Email    string `json:"email"`
+			Password string `json:"password"`
+		}
+
+		var request EmailRegisterRequest
+
+		if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+			response.BadRequest(w, "Invalid request")
+			return
+		}
+
+		if request.Email == "" {
+			response.BadRequest(w, "Missing required field: email")
+			return
+		}
+
+		if request.Password == "" {
+			response.BadRequest(w, "Missing required field: password")
+			return
+		}
+
+		userParams := sugar.CreateUserParams{
+			Email:    request.Email,
+			Password: request.Password,
+		}
+
+		user, err := h.queries.CreateUser(r.Context(), userParams)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+
+		response.Success(w, "Successfully registered user.", nil)
 	}
 }
--- api/helpers/response/response.go
+++ api/helpers/response/response.go
@@ -17,3 +17,27 @@ func BadRequest(w http.ResponseWriter, message string) {
 		Data:    nil,
 	})
 }
+
+func InternalServerError(w http.ResponseWriter, err error, message string) {
+
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&types.ApiResponse{
+		Code:    http.StatusInternalServerError,
+		Message: message,
+		Success: false,
+		Data:    nil,
+	})
+}
+
+func Success(w http.ResponseWriter, message string, data interface{}) {
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&types.ApiResponse{
+		Code:    http.StatusOK,
+		Message: message,
+		Success: true,
+		Data:    data,
+	})
+}
